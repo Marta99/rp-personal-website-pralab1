b4378891c5e49fcbf1703a21a60919ee28932bfc
Marta <marta.albets@gmail.com>
2020-02-11 12:17:15 +0100

    settings changed

--- personal_website/urls.py
+++ personal_website/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('first-app/', include('hello_world.urls'))
+    path('first-app/', include('apps.hello_world.urls'))
 ]
